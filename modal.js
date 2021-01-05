dc9d5ef9decbae16708cb4e618704df3bb17d924
Michel Sakkas <mirosa06@yahoo.com>
2021-01-05 23:14:04 +0100

    close modal function

--- modal.js
+++ modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground")
 const modalBtn = document.querySelectorAll(".modal-btn")
 const formData = document.querySelectorAll(".formData")
 
+const closeModalBtn = document.querySelectorAll(".close")
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
 
@@ -20,6 +22,13 @@ function launchModal() {
     modalbg.style.display = "block"
 }
 
+//  close modal
+function closeModal() {
+    modalbg.style.display = "none"
+}
+closeModalBtn.forEach((close) => close.addEventListener("click", closeModal))
+
+// Fields validation
 document
     .getElementById("btn-submit")
     .addEventListener("click", function (event) {
